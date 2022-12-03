4b707312db3e53aca3e2c0e3421429732782cc8c
Alexandru Tabara <alexandru.tab1@gmail.com>
2022-12-03 16:28:55 +0200

    Add project files.

---
+++ lab2ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab2ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex 1: Scrieti un program care va afisa suma cifrelor unui numar n, n fiind citit de la tastatura.
+            int n, suma = 0, m;
+
+            Console.WriteLine("Introduceti un numar");
+
+            n = int.Parse(Console.ReadLine());
+
+            while (n>0)
+            {
+                m = n % 10;
+                suma = suma + m;
+                n = n / 10;
+            }
+            Console.WriteLine("Suma cifrelor este " + suma);
+
+        }
+    }
+}
